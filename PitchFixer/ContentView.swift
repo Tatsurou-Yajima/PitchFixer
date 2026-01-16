25077275956027acc0a682269ca2a2e84226bc0b
dmmboost-tyajima <yajima-tatsuro@kcgrp.jp>
2026-01-16 19:49:42 +0900

    Initial Commit

---
+++ PitchFixer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PitchFixer
+//
+//  Created by 矢島 達朗 on 2026/01/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PitchFixer/PitchFixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchFixerApp.swift
+//  PitchFixer
+//
+//  Created by 矢島 達朗 on 2026/01/16.
+//
+
+import SwiftUI
+
+@main
+struct PitchFixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
